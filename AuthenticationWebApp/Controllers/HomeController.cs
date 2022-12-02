1c177cbcebae332811413db3def026dd07800d97
Aggelos <aggelos.gkampis@gmail.com>
2022-12-02 16:06:03 +0200

    Adding Authorization and Roles

---
+++ AuthenticationWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationWebApp.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+        public ActionResult About()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                return Content("Hello Administrator ! ! !");
+            }
+            else
+            {
+                return Content("Hello Random");
+            }
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AuthenticationWebApp/Migrations/Configuration.cs
@@ -0,0 +1,67 @@
+﻿namespace AuthenticationWebApp.Migrations
+{
+    using AuthenticationWebApp.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AuthenticationWebApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(AuthenticationWebApp.Models.ApplicationDbContext context)
+        {
+            if (!context.Roles.Any(x => x.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager  = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole() { Name = "Admin" };
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(x => x.Name == "User"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole() { Name = "User" };
+                manager.Create(role);
+            }
+
+            var passwordHash = new PasswordHasher();
+            if (!context.Users.Any( x=> x.UserName == "passno9000@hotmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser()
+                {
+                    UserName = "passno9000@hotmail.com",
+                    Email = "passno9000@hotmail.com",
+                    PasswordHash = passwordHash.HashPassword("192!Osfp192")
+                };
+
+                manager.Create(user);
+                manager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(x => x.UserName == "192osfp192@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser()
+                {
+                    UserName = "192osfp192@gmail.com",
+                    Email = "192osfp192@gmail.com",
+                    PasswordHash = passwordHash.HashPassword("192!Osfp192")
+                };
+
+                manager.Create(user);
+                manager.AddToRole(user.Id, "User");
+            }
+        }
+    }
+}
